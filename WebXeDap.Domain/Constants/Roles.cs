10e23a344fa3561a4b969befa278e0c190472db5
Nguyen Quang Huy <huykhongcan04@gmail.com>
2026-04-30 16:04:54 +0700

    feat(domain): add roles constants

---
+++ WebXeDap.Domain/Constants/Roles.cs
@@ -0,0 +1,8 @@
+namespace WebXeDap.Domain.Constants;
+
+public static class ROLES
+{
+	public const string ADMIN = "Admin";
+	public const string STAFF = "Staff";
+	public const string CUSTOMER = "Customer";
+}
