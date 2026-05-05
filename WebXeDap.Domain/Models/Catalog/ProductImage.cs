9c530d273ac793ae12e740366f91daced948cd57
Nguyen Quang Huy <huykhongcan04@gmail.com>
2026-05-06 02:14:05 +0700

    refactor(domain): fix productimage key typo

--- WebXeDap.Domain/Models/Catalog/ProductImage.cs
+++ WebXeDap.Domain/Models/Catalog/ProductImage.cs
@@ -3,7 +3,7 @@ namespace WebXeDap.Domain.Models;
 public class ProductImage
 {
 	public int ID { get; set; }
-	public required string key { get; set; }
+	public required string Key { get; set; }
 	public int Order { get; set; }
 	public int ProductID { get; set; }
 	public required Product Product { get; set; }
