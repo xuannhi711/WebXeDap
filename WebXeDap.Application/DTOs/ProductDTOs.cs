6893288b86ccf76e16b68e18c94360d21c1457c0
Nguyen Quang Huy <huykhongcan04@gmail.com>
2026-05-06 16:33:26 +0700

    feat(application): add product, productimage dtos

--- WebXeDap.Application/DTOs/ProductDTOs.cs
+++ WebXeDap.Application/DTOs/ProductDTOs.cs
@@ -1,31 +1,44 @@
+using WebXeDap.Application.Extensions;
+
 namespace WebXeDap.Application.DTOs;
 
 public record CreateProductRequest(
-    string Name,
-    string? Description,
-    decimal Price,
-    int Quantity,
-    List<int>? CategoryIDs,
-    string? CurrencySymbol = "VNĐ"
+	string Name,
+	string? Description,
+	decimal Price,
+	int Quantity,
+	int[]? CategoryIDs,
+	string? CurrencySymbol = "VNĐ"
 );
 
 public record UpdateProductRequest(
-    int ID,
-    string? Name,
-    string? Description,
-    decimal? Price,
-    int? Quantity,
-    List<int>? CategoryIDs,
-    string? CurrencySymbol
+	int ID,
+	string? Name,
+	string? Description,
+	decimal? Price,
+	int? Quantity,
+	int[]? CategoryIDs,
+	string? CurrencySymbol
+);
+
+public record FilterProductRequest(
+	string? Keyword,
+	int[]? CategoryIDs,
+	decimal? MinPrice,
+	decimal? MaxPrice,
+	string? SortBy,
+	int Page = 1,
+	int Size = 20,
+	Order Order = Order.ASCENDING
 );
 
 public record ProductResponse(
-    int ID,
-    string Name,
-    string Description,
-    decimal Price,
-    int Quantity,
-    List<CategoryResponse> Categories,
-    List<ProductImageResponse> Images,
-    string CurrencySymbol
+	int ID,
+	string Name,
+	string Description,
+	decimal Price,
+	int Quantity,
+	List<CategoryResponse> Categories,
+	List<ProductImageResponse> Images,
+	string CurrencySymbol
 );
--- WebXeDap.Application/DTOs/ProductImageDTOs.cs
+++ WebXeDap.Application/DTOs/ProductImageDTOs.cs
@@ -1,13 +1,5 @@
 namespace WebXeDap.Application.DTOs;
 
-public record CreateProductImageRequest(
-    int ProductID,
-    string Key,
-    int Order
-);
+public record CreateProductImageRequest(int ProductID, string Key, int Order);
 
-public record ProductImageResponse(
-    int ID,
-    string URL,
-    int Order
-);
+public record ProductImageResponse(int ID, string URL, int Order);
