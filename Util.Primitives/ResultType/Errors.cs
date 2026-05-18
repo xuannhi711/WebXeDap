bed3663296a6eeecfcfe2637e59452b171dea26d
Nguyen Quang Huy <huykhongcan04@gmail.com>
2026-05-18 19:51:14 +0700

    feat(primitives): impl tostring for errors

--- Util.Primitives/ResultType/Errors.cs
+++ Util.Primitives/ResultType/Errors.cs
@@ -1,9 +1,34 @@
+using System.Text.Json;
+
 namespace Util.Primitives.ResultType;
 
 public abstract record Error;
 
-public record NotFoundError(string Message) : Error;
+public record NotFoundError(string Message) : Error
+{
+    public override string ToString()
+    {
+        return $"Not found: {Message}";
+    }
+};
 
-public record UnknownError(string Message) : Error;
+public record UnknownError(string Message) : Error
+{
+    public override string ToString()
+    {
+        return $"Unknown error: {Message}";
+    }
+};
 
-public record ValidationError(IDictionary<string, string[]> Errors) : Error;
+public record ValidationError(IDictionary<string, string[]> Errors) : Error
+{
+    private static readonly JsonSerializerOptions JsonOptions = new()
+    {
+        WriteIndented = true,
+    };
+
+    public override string ToString()
+    {
+        return $"Validation errors: {JsonSerializer.Serialize(Errors, JsonOptions)}";
+    }
+}
